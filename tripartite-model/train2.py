c2d8083579b851fe7d14ec706928701310b055a0
Michael Goodale <michael.goodale@mail.mcgill.ca>
2022-09-29 03:15:07 +0200

    Removed one relu

--- tripartite-model/train2.py
+++ tripartite-model/train2.py
@@ -148,7 +148,7 @@ def train_model(alpha=0.9, dim=2, k=2, n_hidden=32, n_couplings=16,
 
             _, log_probs = model.transform(features, concepts[pos_target], with_log_probs=True)
             pos_loss = (-log_probs).mean()
-            neg_loss = F.relu(math.log(0.5) - model(features.repeat_interleave(NEG_SAMPLING, 0), concepts[neg_samples.view(-1)], negative_example=True)).mean()
+            neg_loss = -model(features.repeat_interleave(NEG_SAMPLING, 0), concepts[neg_samples.view(-1)], negative_example=True).mean()
             real_loss = pos_loss + neg_loss
 
             # Sample from each distribution and pass to negative of different.
